80d00ab18405e29cbbd1a97946777309f8613a37
jasdasd <asdasd@asdasd.com>
2020-11-27 06:51:15 +0700

    move check round to backend

--- controller/TicketController.js
+++ controller/TicketController.js
@@ -8,20 +8,32 @@ const RoundModel = mongoose.model("RoundModel");
 exports.Buy = async function (req, res) {
     var userId = req.body.userId;
     var ticket = req.body.ticket;
-    var roundId = req.body.roundId;
-    var checkRound = await RoundModel.findOne({ roundId: roundId });
+    let timeNow = Date.now()
+    var checkRound = await RoundModel.findOne({
+        $and: [
+            {
+                "roundStartTime": { $lte: timeNow }
+            },
+            {
+                "roundEndTime": { $gt: timeNow }
+            }
+        ]
+    });
 
+    // console.log(checkRound);
+    if (!checkRound) {
+        console.log("null checkRound");
+        return res.status(400).json({ error: "cannot found round id" })
+    }
+    let roundId = checkRound.roundId
 
     if (!userId) {
         res.status(400).json({ error: "Missing userId" })
-    } else if (!checkRound) {
-        res.status(400).json({ error: "cannot found round id" })
     } else if (checkRound.active !== true) {
         res.status(400).json({ error: "You cannot buy ticket at this time" })
     }
     else if (!ticket) {
         res.status(400).json({ error: "Ticket must be not null" })
-
     }
     else if (ticket > 1) {
         res.status(400).json({ error: "Too fast" })
