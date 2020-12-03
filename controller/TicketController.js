bbf63037bf87cba4d94185fcbf762538b30d78ed
Tuan Anh <tuananh@gmail.com>
2020-12-03 12:02:13 -0800

    add queue when buy ticket

--- controller/TicketController.js
+++ controller/TicketController.js
@@ -4,23 +4,65 @@ const TicketService = require('../middle/TIcketService')
 const UserModel = mongoose.model("UserModel");
 const RoundModel = mongoose.model("RoundModel");
 
+class CQueue {
+    constructor() {
+      this.data = [];
+      this.size = 0;
+      this.front = 0;
+      this.rear = 0;
+    }
+  
+    isEmpty() {
+      return this.size === 0;
+    }
+  
+    enqueue(item) {
+
+      this.data[this.rear] = item;
+      this.size++;
+      return true;
+    }
+  
+    dequeue() {
+      if (this.isEmpty()) return undefined;
+  
+      let item = this.data[this.front];
+      this.size--;
+      return item;
+    }
+  
+    front() {
+      if (this.isEmpty()) return undefined;
+  
+      return this.data[0];
+    }
+  
+    rear() {
+      if (this.isEmpty()) return undefined;
+  
+      return this.data[this.size - 1];
+    }
+
+}
 
 exports.Buy = async function (req, res) {
+    const queue = new CQueue()
     var userId = req.body.userId;
     var ticket = req.body.ticket;
+    
     let timeNow = Date.now()
     var checkRound = await RoundModel.findOne({
         $and: [{
             "roundStartTime": { $lte: timeNow }
         }, { "roundEndTime": { $gt: timeNow } }]
     });
-
     // console.log(checkRound);
     if (!checkRound) {
         console.log("null checkRound");
         return res.status(400).json({ error: "cannot found round id" })
     }
     let roundId = checkRound.roundId
+    queue.enqueue({valTicket:ticket, userId, roundId})
 
     if (!userId) {
         res.status(400).json({ error: "Missing userId" })
@@ -38,7 +80,8 @@ exports.Buy = async function (req, res) {
     }
     else {
         try {
-            TicketService.UpdateTicket({ valTicket: ticket, userId, roundId })
+            let value = queue.dequeue()
+            TicketService.UpdateTicket(value)
         } catch (err) {
             console.log("err in Updateticket", err);
             return res.status(400).json({ message: "something went wrong" })
